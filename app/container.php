76283d82d9e4995bb67e18bfacbb3c1f5bcbfaec
james-chege <jymeschege@gmail.com>
2018-04-10 22:04:24 +0300

    Add items

--- app/container.php
+++ app/container.php
@@ -2,12 +2,18 @@
 
 use function DI\get;
 use Slim\Views\Twig;
+use Cart\Basket\Basket;
 use Cart\Models\Product;
 use Slim\Views\TwigExtension;
 use Interop\Container\ContainerInterface;
+use Cart\Support\Storage\SessionStorage;
+use Cart\Support\Storage\Contracts\StorageInterface;
 
 return [
 	'router' => get(Slim\Router::class),
+	StorageInterface::class => function (ContainerInterface $c) {
+		return new SessionStorage('cart');
+	},
 	Twig::class => function (ContainerInterface $c){
 		$twig = new Twig(__DIR__ . '/../resources/views', [
 			'cache' => false
@@ -17,10 +23,19 @@ return [
 			$c->get('router'),
 			$c->get('request')->getUri()
 		));
+
+		$twig->getEnvironment()->addGlobal('basket', $c->get(Basket::class));
+
 		return $twig;
 	},
 	Product::class => function (ContainerInterface $c) {
 		return new Product;
 	},
+	Basket::class => function(ContainerInterface $c) {
+		return new Basket(
+			$c->get(SessionStorage::class),
+			$c->get(Product::class)
+		);
+	}
  ];
 
